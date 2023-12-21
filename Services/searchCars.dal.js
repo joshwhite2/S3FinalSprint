281b3079546c0ced3e1e44b962fdacd37d2fb5ea
joshwhite2 <126173462+joshwhite2@users.noreply.github.com>
2023-12-21 13:39:55 -0330

    CSS, working login

--- Services/searchCars.dal.js
+++ Services/searchCars.dal.js
@@ -1,11 +1,24 @@
-// function that combines postgres and mongo queries to search for cars
-const logSearchAction = require('./src/routes/logging'); // Import the logging function
- 
-const searchCars = async (userId, search) => {
+// function that combines postgres and mongo queries to search for cars and logs the search parameters 
+// Import the logging function
+import logSearchAction from logging.js
+//  const fs=require('fs');
+//  const path = require('path');
+//  const logSearchAction = (search, timestamp, username) => {
+//   const log = `${timestamp} - User name: ${username}, Search: ${search}\n`;
+//   console.log('Logging search:', log);
+//   const filePath = path.join(__dirname, '../../src/routes/searchLog.txt');
+//   fs.appendFile(filePath, log, (error) => {
+//     if (error) {
+//       console.error('Error writing to searchLog.txt:', error);
+//     }
+//   });
+// };
+
+const searchCars = async (username, search) => {
   try {
     // Log the search
     const timestamp = new Date().toISOString();
-    logSearchAction(userId, search, timestamp);
+    logSearchAction(search, timestamp, username);
     // Search in PostgreSQL
     const postgresResult = await getCarsPostgres(search);
 
@@ -14,10 +27,11 @@ const searchCars = async (userId, search) => {
 
     // Combine the results
     const combinedResult = [...postgresResult, ...mongoResult];
-
+  
     return combinedResult;
   } catch (error) {
     console.error(error);
+    
     throw error;
   }
 };
